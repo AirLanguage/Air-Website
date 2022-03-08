072a80866d92f70bfb3932bef748296bf970b094
Mateusz <mateuszslotwinski16@gmail.com>
2022-03-08 23:14:15 +0100

    links

--- src/components/Home/Hero/Hero.component.tsx
+++ src/components/Home/Hero/Hero.component.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import NonStyledLink from 'next/link'
+import NextLink from '@/layout/material-ui/link'
 import Stack from '@mui/material/Stack'
+import Avatar from '@mui/material/Avatar'
 import Button from '@mui/material/Button'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
 
 import UseStyles from './Hero.styles'
+import { Pages } from '@/components/Shared/Navbar/Navbar.data'
 
 const Navbar: React.FC = () => {
   const styles = UseStyles()
@@ -10,24 +16,72 @@ const Navbar: React.FC = () => {
     <div className={styles.section}>
       <div className={styles.container}>
         <div className={styles.wrapper}>
-          <div className={styles.topWrapper}></div>
-          <div className={styles.mainWrapper}>
-            <div className={styles.title}>AIR</div>
-            <div className={styles.slogan}>
-              Easy, efficient and incredibly enjoyable open-source programming
-              language. See for yourself how easy it is!
+          <Grid
+            container
+            className={styles.topWrapper}
+            alignItems="center"
+            columnSpacing={{ xs: 0 }}
+          >
+            <div className={styles.avatar}>
+              <Typography
+                variant="h5"
+                component="div"
+                sx={{
+                  flexGrow: 1,
+                  fontWeight: 600,
+                  fontFamily: 'Ethnocentric',
+                  fontSize: 42,
+                }}
+              >
+                <NextLink href="/" color="inherit" underline="none">
+                  AIR
+                </NextLink>
+              </Typography>
             </div>
-            <Stack spacing={2} direction="row">
-              <Button variant="contained" size="large">
-                Get Started
-              </Button>
-              <Button variant="outlined" color="inherit" size="large">
-                Documentation
-              </Button>
-              <Button variant="outlined" color="inherit" size="large">
-                Download
-              </Button>
-            </Stack>
+            {Pages.map((page) => (
+              <Grid
+                item
+                columnSpacing="30"
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                className={styles.topWrapperItem}
+                key={page.text}
+              >
+                <NextLink color="inherit" underline="none" href={page.link}>
+                  <Typography className={styles.link}>{page.text}</Typography>
+                </NextLink>
+              </Grid>
+            ))}
+          </Grid>
+          <div className={styles.mainWrapper}>
+            <div>
+              <div className={styles.title}>AIR</div>
+              <div className={styles.slogan}>
+                Easy, efficient and incredibly enjoyable open-source programming
+                language. See for yourself how easy it is!
+              </div>
+              <Stack
+                direction={{ xs: 'column', sm: 'row' }}
+                spacing={{ xs: 1, sm: 2, md: 4 }}
+              >
+                <NonStyledLink href="/start" passHref>
+                  <Button variant="contained" size="large">
+                    Get Started
+                  </Button>
+                </NonStyledLink>
+                <NonStyledLink href="/docs" passHref>
+                  <Button variant="outlined" color="inherit" size="large">
+                    Documentation
+                  </Button>
+                </NonStyledLink>
+                <NonStyledLink href="/download" passHref>
+                  <Button variant="outlined" color="inherit" size="large">
+                    Download
+                  </Button>
+                </NonStyledLink>
+              </Stack>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Home/Hero/Hero.styles.ts
+++ src/components/Home/Hero/Hero.styles.ts
@@ -1,6 +1,5 @@
 import { makeStyles } from '@mui/styles'
 import Panda from '@/assets/home/panda.jpg'
-import BlurPanda from '@/assets/home/blur.jpg'
 
 const useStyles = makeStyles(() => ({
   section: {
@@ -21,14 +20,39 @@ const useStyles = makeStyles(() => ({
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    backdropFilter: 'brightness(0.9)',
+    backdropFilter: 'brightness(0.7)',
+    padding: 10,
+  },
+  avatar: {
+    flexGrow: 1,
   },
   wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
     width: 1200,
   },
-  topWrapper: {},
+  topWrapper: {
+    paddingTop: 10,
+  },
+  topWrapperItem: {
+    paddingRight: 15,
+  },
+  link: {
+    fontSize: 19,
+    textTransform: 'uppercase',
+    transition: '0.2s all',
+    fontWeight: 500,
+    '&:hover': {
+      color: '#00afdb',
+    },
+  },
   mainWrapper: {
-    paddingTop: 30,
+    flexGrow: 1,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: '#e3e3e3',
   },
   title: {
     fontFamily: 'Ethnocentric',
@@ -38,10 +62,9 @@ const useStyles = makeStyles(() => ({
   slogan: {
     fontSize: 24,
     lineHeight: '24px',
-    width: 700,
+    maxWidth: 700,
     padding: '20px 0',
   },
-  buttons: {},
 }))
 
 export default useStyles
